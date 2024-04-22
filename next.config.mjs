3136e886a1d56086d3590db0916ec79c9d2fd4e8
AngeloCarnevale <angelo.carnevale@hotmail.com>
2024-04-22 15:29:24 -0300

    refactor: change configs to work with a monorepo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    transpilePackages: [
+        '@smarthub/ui',
+    ],
+};
 
 export default nextConfig;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
-};
+const config = require('@repo/tailwind-config/tailwind.config')
 
 export default config;
